f5506576dc90b79502d2b3869aec0fef30f8be25
syuparn <s.hello.spagetti@gmail.com>
2023-01-08 17:05:07 +0900

    impl table

--- lib/issues/table.ex
+++ lib/issues/table.ex
@@ -1,6 +1,6 @@
 defmodule Issues.Table do
   def generate_issue_table(issues) do
-    generate_table([], issue_column_names())
+    generate_table(issue_rows(issues), issue_column_names())
   end
 
   def generate_table(rows, column_names) do
@@ -8,10 +8,27 @@ defmodule Issues.Table do
 
     [
       generate_header(column_names, widths),
-      generate_spacer(widths)
+      generate_spacer(widths),
+      generate_rows(rows, widths)
     ]
     |> Enum.join("\n")
-    |> then(&(&1 <> "\n")) # add trailing breakline
+    # add trailing breakline
+    |> then(&(&1 <> "\n"))
+  end
+
+  def issue_rows(issues) do
+    Enum.map(issues, &issue_row/1)
+  end
+
+  def issue_column_keys() do
+    ["number", "created_at", "title"]
+  end
+
+  def issue_row(issue) do
+    issue_column_keys()
+    |> Enum.map(fn key ->
+      to_string(issue[key])
+    end)
   end
 
   def issue_column_names() do
@@ -40,11 +57,26 @@ defmodule Issues.Table do
     |> Stream.zip()
     |> Stream.map(fn {column_name, width} -> String.pad_trailing(column_name, width) end)
     |> Enum.join(" | ")
+    |> String.trim_trailing()
   end
 
   def generate_spacer(widths) do
     widths
-    |> Stream.map(&(String.duplicate("-", &1)))
+    |> Stream.map(&String.duplicate("-", &1))
     |> Enum.join("-+-")
+  end
+
+  def generate_rows(rows, widths) do
+    rows
+    |> Enum.map(&generate_row(&1, widths))
+    |> Enum.join("\n")
+  end
+
+  def generate_row(row, widths) do
+    [row, widths]
+    |> Stream.zip()
+    |> Stream.map(fn {row, width} -> String.pad_trailing(row, width) end)
+    |> Enum.join(" | ")
+    |> String.trim_trailing()
   end
 end
--- test/table_test.exs
+++ test/table_test.exs
@@ -2,7 +2,7 @@ defmodule TableTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.Table, only: [generate_issue_table: 1]
+  import Issues.Table, only: [generate_issue_table: 1, issue_rows: 1]
 
   test "generate an issue table with no issues" do
     issues = fake_issues([])
@@ -10,7 +10,45 @@ defmodule TableTest do
     assert generate_issue_table(issues) == """
            # | created_at | title
            --+------------+------
+
            """
+  end
+
+  test "generate an issue table with one issue" do
+    issues =
+      fake_issues([
+        {123, "2023-01-01-T01:23:45Z", "Simple bug"}
+      ])
+
+    assert generate_issue_table(issues) == """
+           #   | created_at            | title
+           ----+-----------------------+-----------
+           123 | 2023-01-01-T01:23:45Z | Simple bug
+           """
+  end
+
+  test "generate an issue table with multiple issues" do
+    issues =
+      fake_issues([
+        {999, "2023-01-01-T01:23:45Z", "Simple bug"},
+        {1000, "2023-01-02-T01:23:45Z", "Cool feature"}
+      ])
+
+    assert generate_issue_table(issues) == """
+           #    | created_at            | title
+           -----+-----------------------+-------------
+           999  | 2023-01-01-T01:23:45Z | Simple bug
+           1000 | 2023-01-02-T01:23:45Z | Cool feature
+           """
+  end
+
+  test "issue_rows extracts specific keys from map" do
+    issues =
+      fake_issues([
+        {123, "2023-01-01-T01:23:45Z", "Simple bug"}
+      ])
+
+    assert issue_rows(issues) == [["123", "2023-01-01-T01:23:45Z", "Simple bug"]]
   end
 
   defp fake_issues(values) do
