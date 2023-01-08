c811192fd77e303b2206c89aea01ed3317fb3c48
syuparn <s.hello.spagetti@gmail.com>
2023-01-08 14:56:22 +0900

    add process

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -7,7 +7,9 @@ defmodule Issues.CLI do
   """
 
   def run(argv) do
-    parse_args(argv)
+    argv
+      |> parse_args
+      |> process
   end
 
   @doc """
@@ -34,5 +36,16 @@ defmodule Issues.CLI do
 
   def args_to_internal_representation(_) do # return :help by default
     :help
+  end
+
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GitHubIssues.fetch(user, project)
   end
 end
---
+++ lib/issues/github_issues.ex
@@ -0,0 +1,5 @@
+defmodule Issues.GithubIssues do
+  def fetch(_user, _project) do
+    # TODO: impl
+  end
+end
