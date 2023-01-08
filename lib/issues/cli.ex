5be9df05562312c7ed3ed5eaea6437def055fc82
syuparn <s.hello.spagetti@gmail.com>
2023-01-08 15:31:37 +0900

    sort result

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -50,11 +50,19 @@ defmodule Issues.CLI do
   def process({user, project, _count}) do
     Issues.GitHubIssues.fetch(user, project)
     |> decode_response()
+    |> sort_into_descending_order()
   end
 
   def decode_response({:ok, body}), do: body
   def decode_response({:error, error}) do
     IO.puts "Error fetching from GitHub: #{error["message"]}"
     System.halt(2)
+  end
+
+  def sort_into_descending_order(issues) do
+    issues
+    |> Enum.sort(fn i1, i2 ->
+      i1["created_at"] >= i2["created_at"]
+    end)
   end
 end
